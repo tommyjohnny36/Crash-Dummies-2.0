a027723e274f2a5e26f355ad045a3d986f589b82
TJ Lawless <87883465+tommyjohnny36@users.noreply.github.com>
2022-01-10 19:04:09 -0500

    Update table_queries.sql

---
+++ table_queries.sql
@@ -0,0 +1,27 @@
+-- Visualizations --
+
+-- 1. List the following details of each accident involving vehicle
+-- accidents: state, case_num, county, city, lat, lon, first harmful event, manner of collision, route, rural/urban, weather
+-- people: vehicle_manufacturer, vehicle_model, doa_status
+-- vehicle: body type
+
+-- Vehicle types involved in most accidents
+-- Vehicle type involved in most fatal accidents
+-- Vehicle type involved with most non-fatal accidents
+
+-- 2. List the following details of each accident involving age group
+-- people: age, sex, doa_status, age_label
+-- accidents: persons, county, city, month, day, lat, lon, first harmful event, manner of collision, route, rural/urban, weather
+-- vehicle: speed exceed, driver safety
+
+-- age group involved in most accidents
+-- age group involved with most fatal accidents
+-- age group involved with most non-fatal accidents
+
+
+-- 3. List the following details of each accident involving sex of driver
+-- accidents: persons, county, city, month, day, lat, lon, first harmful event & manner of collision by route and rural/urban, weather
+-- people: sex, doa_status, age, count of fatal and non-fatal accidents by month and day
+-- vehicle: speed exceed, driver safety, hit/run
+
+
